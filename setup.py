ddca2243476d8188992f0fe807ff3bc02feac0cb
Hugo van Kemenade <hugovk@users.noreply.github.com>
2023-01-27 19:14:23 +0200

    Move to src layout

--- setup.py
+++ setup.py
@@ -9,15 +9,17 @@ dconv_includes = [
     dir
     for dir in environ.get(
         "UJSON_BUILD_DC_INCLUDES",
-        "./deps/double-conversion/double-conversion",
+        "./src/deps/double-conversion/double-conversion",
     ).split(pathsep)
     if dir
 ]
 dconv_libs = shlex.split(environ.get("UJSON_BUILD_DC_LIBS", ""))
 dconv_source_files = []
 if not dconv_libs:
-    dconv_source_files.extend(glob("./deps/double-conversion/double-conversion/*.cc"))
-dconv_source_files.append("./lib/dconv_wrapper.cc")
+    dconv_source_files.extend(
+        glob("./src/deps/double-conversion/double-conversion/*.cc")
+    )
+dconv_source_files.append("./src/lib/dconv_wrapper.cc")
 
 if platform.system() == "Linux" and environ.get("UJSON_BUILD_NO_STRIP", "0") not in (
     "1",
@@ -31,18 +33,18 @@ module1 = Extension(
     "ujson",
     sources=dconv_source_files
     + [
-        "./python/ujson.c",
-        "./python/objToJSON.c",
-        "./python/JSONtoObj.c",
-        "./lib/ultrajsonenc.c",
-        "./lib/ultrajsondec.c",
+        "./src/python/ujson.c",
+        "./src/python/objToJSON.c",
+        "./src/python/JSONtoObj.c",
+        "./src/lib/ultrajsonenc.c",
+        "./src/lib/ultrajsondec.c",
     ],
-    include_dirs=["./python", "./lib"] + dconv_includes,
+    include_dirs=["./src/python", "./src/lib"] + dconv_includes,
     extra_compile_args=["-D_GNU_SOURCE"],
     extra_link_args=["-lstdc++", "-lm"] + dconv_libs + strip_flags,
 )
 
-with open("python/version_template.h") as f:
+with open("src/python/version_template.h") as f:
     version_template = f.read()
 
 
@@ -56,7 +58,7 @@ setup(
     ext_modules=[module1],
     use_scm_version={
         "local_scheme": local_scheme,
-        "write_to": "python/version.h",
+        "write_to": "src/python/version.h",
         "write_to_template": version_template,
     },
 )
